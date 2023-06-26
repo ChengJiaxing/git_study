b6aca033d5c39d992c8be1d0024439bccb0b5211
ChengJiaxing <15922305689@163.com>
2023-06-26 19:09:04 +0800

    two commit

---
+++ src/main/java/com/example/chengjiaxing/Hello.java
@@ -0,0 +1,12 @@
+package com.example.chengjiaxing;
+
+/**
+ * Author : Cheng Jiaxing
+ * Date: 2023/6/26 19:08
+ * Description:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
