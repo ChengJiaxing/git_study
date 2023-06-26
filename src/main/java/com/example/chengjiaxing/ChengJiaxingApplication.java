d4c6a192b7a6d816f02feb0bb0c2a05955d6191e
ChengJiaxing <15922305689@163.com>
2023-06-26 19:05:59 +0800

    first commit

---
+++ src/main/java/com/example/chengjiaxing/ChengJiaxingApplication.java
@@ -0,0 +1,13 @@
+package com.example.chengjiaxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChengJiaxingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChengJiaxingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chengjiaxing/ChengJiaxingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chengjiaxing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChengJiaxingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
